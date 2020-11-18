2b29f4c0c9534c17d9980dbadeddfc64c2c72ef4
Furkan Sevgili <fs.2838@gmail.com>
2020-11-18 20:53:14 +0300

    Add IService

---
+++ NLayerProject.Core/Service/IService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerProject.Core.Service
+{
+    interface IService<TEntity> where TEntity:class
+    {
+        Task<TEntity> GetByIdAsync(int Id);
+
+        Task<IEnumerable<TEntity>> GetAllAsync();
+
+        Task<IEnumerable<TEntity>> Find(Expression<Func<TEntity, bool>> predicate);
+
+        Task<TEntity> SingleOrDefaultAsync(Expression<Func<TEntity, bool>> predicate);
+
+        Task AddAsync(TEntity entity);
+
+        Task AddRangeAsync(IEnumerable<TEntity> entities);
+
+        void Remove(TEntity entity);
+
+        void RemoveRange(TEntity entity);
+
+        TEntity Update(TEntity entity);
+    }
+}
