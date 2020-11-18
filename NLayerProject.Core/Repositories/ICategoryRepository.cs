d866871f72f517306a2bcf44c641c839cf68d7ba
Furkan Sevgili <fs.2838@gmail.com>
2020-11-18 19:25:50 +0300

    ICategoryRepository Generic Interface

---
+++ NLayerProject.Core/Repositories/ICategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using NLayerProject.Core.Models;
+
+namespace NLayerProject.Core.Repositories
+{
+    interface ICategoryRepository:IRepository<Category>
+    {
+        Task<Category> GetWithProductsByIdAsync(int categoryId);
+
+
+    }
+}
