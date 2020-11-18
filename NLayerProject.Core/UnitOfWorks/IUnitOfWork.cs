aae8e4a44a7c6089429d7d2c0fc7752ac6b69cf3
Furkan Sevgili <fs.2838@gmail.com>
2020-11-18 21:19:04 +0300

    Add IUnitOfWork

---
+++ NLayerProject.Core/UnitOfWorks/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using NLayerProject.Core.Repositories;
+
+namespace NLayerProject.Core.UnitOfWorks
+{
+    public interface IUnitOfWork
+    {
+        IProductRepository Products { get; }
+        ICategoryRepository Categories { get; }
+        Task CommitAsync();
+        void Commit();
+    }
+}
