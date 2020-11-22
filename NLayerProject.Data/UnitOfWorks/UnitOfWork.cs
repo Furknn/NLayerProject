cc2d3372edc49faa1c775130d3b54db82df014cd
furknn <fs.2838@gmail.com>
2020-11-23 02:29:44 +0300

    Add UnitOfWork.cs

---
+++ NLayerProject.Data/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,31 @@
+using System.Threading.Tasks;
+using NLayerProject.Core.Repositories;
+using NLayerProject.Core.UnitOfWorks;
+using NLayerProject.Data.Repositories;
+
+namespace NLayerProject.Data.UnitOfWorks
+{
+    public class UnitOfWork:IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+        private ProductRepository _productRepository;
+        private CategoryRepository _categoryRepository;
+        public IProductRepository Products => _productRepository??new ProductRepository(_context);
+        public ICategoryRepository Categories => _categoryRepository??new CategoryRepository(_context);
+        
+
+        public UnitOfWork(AppDbContext context)
+        {
+            this._context = context;
+        }
+        public async Task CommitAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
