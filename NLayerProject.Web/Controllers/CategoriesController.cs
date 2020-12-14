f48758490da383e4c5505f733be620b214c9be47
Furkan Sevgili <fs.2838@gmail.com>
2020-12-14 22:51:59 +0300

    Add CategoriesController and Categories Index

---
+++ NLayerProject.Web/Controllers/CategoriesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using NLayerProject.Core.Service;
+using NLayerProject.Web.DTOs;
+
+namespace NLayerProject.Web.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly ICategoryService _categoryService;
+        private readonly IMapper _mapper;
+        public CategoriesController(ICategoryService categoryService,IMapper mapper)
+        {
+            _categoryService = categoryService;
+            _mapper = mapper;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var categories = await _categoryService.GetAllAsync();
+            return View(_mapper.Map<IEnumerable<CategoryDto>>(categories));
+        }
+    }
+}
